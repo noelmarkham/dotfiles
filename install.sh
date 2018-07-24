f00d4a32384b67a8f76f1c058a5199643a029859
Noel Markham <noel.markham@gmail.com>
2018-07-24 13:45:06 +0100

    Add git config to default install script

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ cp -R ~/git/dotfiles/zsh/* ~/.oh-my-zsh/custom
 # Set up Emacs
 rm -rf ~/.emacs.d
 ln -s ~/git/dotfiles/emacs.d/ ~/.emacs.d
+
+# Git config
+ln -fs ~/git/dotfiles/gitconfig ~/.gitconfig
--- zsh/noel.zsh
+++ zsh/noel.zsh
@@ -4,7 +4,8 @@ export ANDROID_HOME=/usr/local/opt/android-sdk
 export PATH=/Applications/Emacs.app/Contents/MacOS:$PATH:/Users/noel/.local/bin
 
 alias sl=ls
-alias weather='curl wttr.in/London'
+alias weather='curl wttr.in/Manchester'
+alias tempdir='cd $(mktemp -d)'
 
 function tab() {
     open -a "Google Chrome" http://$1
