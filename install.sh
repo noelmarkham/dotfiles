17974e6083478fde65f7b70fe76f9d9931828557
Noel Markham <noel.markham@gmail.com>
2018-07-25 09:39:29 +0100

    Set up emacs the same way as the others

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ ln -fs ~/git/dotfiles/zsh/noel.zsh ~/.oh-my-zsh/custom/
 ln -fs ~/git/dotfiles/zsh/themes/noel.zsh-theme ~/.oh-my-zsh/custom/themes/
 
 # Set up Emacs
-rm -rf ~/.emacs.d
-ln -s ~/git/dotfiles/emacs.d/ ~/.emacs.d
+ln -fs ~/git/dotfiles/emacs.d/ ~/.emacs.d
 
 # Git config
 ln -fs ~/git/dotfiles/gitconfig ~/.gitconfig
