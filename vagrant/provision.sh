e30d013924880ad66cfbb68dade5c77036ae0c01
Noel Markham <noel.markham@gmail.com>
2018-07-25 09:11:13 +0100

    Remove prompt for ZSH shell change

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y git zsh
 
-su - vagrant -c 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+su - vagrant -c 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | grep -v shells)"'
 
 chsh -s /bin/zsh vagrant
